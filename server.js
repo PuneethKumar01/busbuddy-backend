c92038eecb7dbc72c11012a4abda2f3264ae42c9
puneethkumar01 <puneethkumar1405@gmail.com>
2025-09-13 06:00:47 +0530

    up

--- server.js
+++ server.js
@@ -20,9 +20,13 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Error:", err));
+
 
 app.use("/api/bus", busRoutes);
 app.use("/api/driver", driverRoutes);
